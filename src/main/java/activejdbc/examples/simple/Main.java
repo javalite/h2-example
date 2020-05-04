04a34e00e5dd319ad79b5a17e3c025ebfc1743fa
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2020-05-04 11:16:07 -0500

    working example

---
+++ src/main/java/activejdbc/examples/simple/Main.java
@@ -0,0 +1,24 @@
+package activejdbc.examples.simple;
+
+import org.javalite.activejdbc.Base;
+import org.javalite.activejdbc.connection_config.DBConfiguration;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Base.open("org.h2.Driver", "jdbc:h2:./test", "sa", "");
+
+        Base.exec("DROP TABLE IF EXISTS users;");
+        Base.exec("CREATE TABLE USERS( id  int(11) NOT NULL auto_increment PRIMARY KEY,     username varchar(255) NOT NULL, password varchar(255));");
+
+        User e = new User();
+        e.set("username", "John");
+        e.set("password", "Doe");
+        e.saveIt();
+
+
+        User.findAll().dump();
+        Base.close();
+    }
+}
---
+++ src/main/java/activejdbc/examples/simple/User.java
@@ -0,0 +1,6 @@
+package activejdbc.examples.simple;
+
+import org.javalite.activejdbc.Model;
+
+public class User extends Model {
+}
